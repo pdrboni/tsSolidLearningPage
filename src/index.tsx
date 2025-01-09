b863e1f143e9e1e6783c0f22846ec8f507a7ee57
Pedro Boni <pedrobg2707@gmail.com>
2025-01-09 19:46:44 -0300

    creating context

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,4 @@
 import { createRoot } from 'react-dom/client';
-import { App } from './App';
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router';
 import { SRP } from './components/srp';
@@ -9,18 +8,23 @@ import { ISP } from './components/isp';
 import { DIP } from './components/dip';
 import { Footer } from './components/Footer';
 import { DarkModeButton } from './components/DarkModeButton';
+import { TitleTSSOLID } from './components/TitleTSSOLID';
+import { ThemeProvider } from './contexts/ThemeContext';
+import './styles/scss/main.scss';
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
-    <DarkModeButton />
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="srp" element={<SRP />} />
-      <Route path="ocp" element={<OCP />} />
-      <Route path="lsp" element={<LSP />} />
-      <Route path="isp" element={<ISP />} />
-      <Route path="dip" element={<DIP />} />
-    </Routes>
-    <Footer />
+    <ThemeProvider>
+      <DarkModeButton />
+      <Routes>
+        <Route path="/" element={<TitleTSSOLID />} />
+        <Route path="/srp" element={<SRP />} />
+        <Route path="/ocp" element={<OCP />} />
+        <Route path="/lsp" element={<LSP />} />
+        <Route path="/isp" element={<ISP />} />
+        <Route path="/dip" element={<DIP />} />
+      </Routes>
+      <Footer />
+    </ThemeProvider>
   </BrowserRouter>,
 );
